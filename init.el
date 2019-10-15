3071243622df33a35c64202e095325a08017844a
Roman Hargrave <roman@hargrave.info>
2019-10-15 14:56:34 -0500

    Pull evil from ELPA, switch to treemacs from neotree

--- init.el
+++ init.el
@@ -11,44 +11,18 @@
 
 (require 'use-package)
 
-(use-package helm
-  :ensure t
-  :config
-  (require 'helm-config))
-
-(use-package general :ensure t)
-(use-package neotree :ensure t)
-
-(use-package doom-themes
-  :ensure t
-  :config
-  (load-theme 'doom-molokai t)
-  (doom-themes-neotree-config)
-  (doom-themes-org-config))
-
-;; XXX remember to run (all-the-icons-install-fonts)
-(use-package doom-modeline
-  :ensure t
-  :hook (after-init . doom-modeline-mode))
-
-; just a ton of modes
-(use-package dockerfile-mode :ensure t :mode "Dockerfile")
-(use-package php-mode        :ensure t :mode "\\.php\\'" :magic "#!/usr/bin/env php")
-
 ; Set font
 (add-to-list 'default-frame-alist '(font . "Source Code Pro-10"))
 (set-face-attribute 'default t :font "Source Code Pro-10")
 
 ; Load evil
-(setq evil-want-keybinding nil)
-(add-to-list 'load-path "~/.emacs.d/evil")
-(require 'evil)
-(evil-mode 1)
-
-; general.el, keymapping
-(require 'general)
-(require 'neotree)
-(require 'sublimity)
+(use-package evil
+  :ensure t
+  :init
+  (setq evil-want-keybinding  nil
+        evil-want-integration t)
+  :config
+  (evil-mode 1))
 
 ; Load keymap fixes for evil
 (use-package evil-collection
@@ -63,25 +37,55 @@
   (define-key evil-normal-state-map (kbd "o") 'evil-forward-char)
   (evil-collection-init))
 
+(use-package helm
+  :ensure t
+  :config
+  (require 'helm-config))
+
+(use-package general :ensure t)
+
+(use-package treemacs :ensure t)
+(use-package treemacs-evil :ensure t)
+
+(use-package doom-themes
+  :ensure t
+  :config
+  (load-theme 'doom-molokai t)
+  (doom-themes-treemacs-config)
+  (doom-themes-org-config))
+
+;; XXX remember to run (all-the-icons-install-fonts)
+(use-package doom-modeline
+  :ensure t
+  :hook (after-init . doom-modeline-mode))
+
+; just a ton of modes
+(use-package dockerfile-mode :ensure t :mode "Dockerfile")
+(use-package php-mode        :ensure t :mode "\\.php\\'" :magic "#!/usr/bin/env php")
+
+
+; general.el, keymapping
+(require 'general)
+
 ; Disable tab auto-insertion
 (setq-default indent-tabs-mode nil)
 
-(setq scroll-step                    1)
-(setq scroll-margin                  9)
-(setq scroll-conservatively          10000)
-(setq mouse-wheel-scroll-amount      '(1 ((shift) . 1)))
-(setq mouse-whell-progressive-speed  nil)
-(setq mouse-whell-follow-mouse       't)
-(setq version-control                t)
-(setq vc-make-backup-files           t)
-(setq vc-follow-symlinks             t)
-(setq coding-system-for-read         'utf-8)
-(setq coding-system-for-write        'utf-8)
-(setq sentence-end-double-space      nil)
-(setq auto-save-file-name-transforms '((".*" "~/.emacs.d/auto-save-list/" t)))
-(setq backup-directory-alist         `(("." . "~/.emacs.d/backups")))
-(setq delete-old-versions            -1)
-(setq custom-file                    "~/.emacs.d/custom.el")
+(setq scroll-step                    1
+      scroll-margin                  9
+      scroll-conservatively          10000
+      mouse-wheel-scroll-amount      '(1 ((shift) . 1))
+      mouse-whell-progressive-speed  nil
+      mouse-whell-follow-mouse       't
+      version-control                t
+      vc-make-backup-files           t
+      vc-follow-symlinks             t
+      coding-system-for-read         'utf-8
+      coding-system-for-write        'utf-8
+      sentence-end-double-space      nil
+      auto-save-file-name-transforms '((".*" "~/.emacs.d/auto-save-list/" t))
+      backup-directory-alist         `(("." . "~/.emacs.d/backups"))
+      delete-old-versions            -1
+      custom-file                    "~/.emacs.d/custom.el")
 
 ;; Enable pair hilighting
 (show-paren-mode 1)
@@ -95,8 +99,9 @@
 
 ;; Stateless global keybindings
 (general-define-key
- "C-c" 'neotree-toggle
- "C-s" 'save-buffer)
+ "C-s"   'save-buffer
+ "A-n"   'evil-next-match
+ "A-N"   'evil-previous-match)
 
 ; Normal mode
 ;; Window movement
@@ -104,22 +109,34 @@
  :states 'normal
  :prefix "C-w"
  "<up>"    'evil-window-up
+ "e"       'evil-window-up
+ "<down>"  'evil-window-down
+ "n"       'evil-window-down
+ "<left>"  'evil-window-left
+ "h"       'evil-window-left
+ "<right>" 'evil-window-right
+ "o"       'evil-window-right)
+
+(general-define-key
+ :states 'normal
+ "SPC t o"   'treemacs
+ "SPC f e x" 'eval-buffer)
+
+;; Tree bindings
+(general-define-key
+ :keymaps  'treemacs-mode-map
+ "SPC t o" 'treemacs
+ "C-c"     'treemacs
+ "RET"     'treemacs-ret-action)
+
+;; because once was not enough
+(general-define-key
+ :keymaps 'treemacs-mode-map
+ :prefix "C-w"
+ "<up>"    'evil-window-up
  "<down>"  'evil-window-down
  "<left>"  'evil-window-left
  "<right>" 'evil-window-right)
-
-(general-define-key
- :states 'normal
- "SPC f e x" 'eval-buffer)
-
-;; Neotree bindings
-(general-define-key
- :states 'normal
- :keymaps 'neotree-mode-map
- "RET" 'neotree-enter
- "C-c" 'neotree-toggle
- "C"   'neotree-change-root
- "R"   'neotree-refresh)
 
 ; set _ to a word character so that C-Left/C-Right/S-Left/S-Right don't skip over it
 (modify-syntax-entry ?_ "w")
