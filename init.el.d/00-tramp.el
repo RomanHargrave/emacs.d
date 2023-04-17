5f3f1ce859e07b58c8cb60cadd1a902f82233d81
Roman Hargrave <roman@hargrave.info>
2023-04-16 20:31:16 -0700

    Don't download tramp, using a non-dist tramp causes problems

--- init.el.d/00-tramp.el
+++ init.el.d/00-tramp.el
@@ -1,5 +1,3 @@
-(use-package tramp
-  :straight (:build t :pre-build (("make" "autoloads")) :no-autoloads t)
-  :config
-  (setq tramp-persistency-file-name
-	(concat user-emacs-directory "tramp")))
+(setq tramp-persistency-file-name
+      (concat user-emacs-directory "tramp"))
+
