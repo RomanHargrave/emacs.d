f3f9fa3e6ba0bd703e19134d2dbd3add52fe9c02
Roman Hargrave <roman@hargrave.info>
2024-10-10 22:16:27 -0700

    Fix company hook

--- init.el.d/01-lsp.el
+++ init.el.d/01-lsp.el
@@ -1,13 +1,19 @@
-(use-package eglot)
+(use-package eglot
+  :demand t
+  :bind (:map eglot-mode-map
+              ("<mouse-3>" . eglot-menu)
+              ("M-v" . eglot-code-actions)
+              ("<f6>" . eglot-code-actions)
+              ("S-<f6>" . eglot-rename)
+              ("S-<f7>" . eglot-find-declaration)))
 
 (use-package company
+  :commands (company-mode global-company-mode)
+  :hook ((rustic-mode go-mode enh-ruby-mode web-mode) . company-mode)
   :bind (([remap indent-for-tab-command] . company-indent-or-complete-common)
          :map company-active-map
          ("<return>" . nil)
          ("RET" . nil)
          ("C-RET" . 'company-complete-selection)
          ("C-<return>" . 'company-complete-selection))
-  :hook (rustic-mode go-mode enh-ruby-mode web-mode)
   :after eglot)
-
-;(use-package flycheck)
