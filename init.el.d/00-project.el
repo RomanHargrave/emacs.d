f4438f03f3a15efb64f5c046c741f52b3c4e3104
Roman Hargrave <roman.hargrave@redcanary.com>
2025-08-27 11:24:55 -0700

    Re-add treemacs

--- init.el.d/00-project.el
+++ init.el.d/00-project.el
@@ -3,6 +3,9 @@
   :config
   (setq magit-save-repository-buffers 'dontask))
 
+(use-package forge
+  :after magit)
+
 (use-package projectile
   :demand t
   :after eglot
@@ -12,3 +15,19 @@
 (use-package editorconfig
   :config
   (editorconfig-mode 1))
+
+(use-package treemacs
+  :bind ((:map treemacs-mode-map
+                ("RET" . treemacs-enter))
+         (:map global-map
+               ("C-," . treemacs-select-window)))
+  :config
+  ;; wtf treemacs, why do you bind RET in global-map???
+  (general-define-key
+   :keymaps 'global
+   "RET" 'newline)
+  (general-define-key
+   :keymaps 'org-mode-map
+   "C-," nil))
+
+(use-package treemacs-projectile)
